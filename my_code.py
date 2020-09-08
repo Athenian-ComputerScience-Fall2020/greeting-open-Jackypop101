952b2b298d6bfb751cc7ba65716c49734678ffc7
Jackypop101 <70411765+Jackypop101@users.noreply.github.com>
2020-09-08 15:59:11 -0700

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -2,3 +2,7 @@
 #  
 
 # Write a program that asks for the user's name and another piece of information.Then prints a response using both of the inputs.
+
+x = input("What is your name?")
+y = input("What is your faviorte color?")
+print("your are", y , x)
